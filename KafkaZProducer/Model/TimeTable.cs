d4a1600860ba3c4ce4cf4b6f12ed8d5bdb903336
zahid <zahid@WINDOWS-7RR77GT>
2023-02-13 22:55:41 -0500

    Add project files.

---
+++ KafkaZProducer/Model/TimeTable.cs
@@ -0,0 +1,30 @@
+﻿using KafkaZProducer.Util;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace KafkaZProducer.Model
+{
+    public class TimeTable
+    {
+        public string key { get; set; }
+        public Time value { get; set; }
+        public void Populate(int time) { 
+            this.key = RandomGenerator.GetContainerKey();
+            this.value = new Time()
+            {
+                time = RandomGenerator.GetTimeKey(time),
+                value = RandomGenerator.GetTimeValue()
+            };
+        }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ KafkaZProducer/Program.cs
@@ -0,0 +1,76 @@
+﻿using Confluent.Kafka;
+using KafkaZProducer.Model;
+using Microsoft.Extensions.Configuration;
+
+namespace KafkaZProducer
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json", optional: false);
+
+            IConfiguration appConfig = builder.Build();
+
+            string brokerList = appConfig.GetConnectionString("Broker");//"localhost:9092"; 
+            string topicName = appConfig.GetConnectionString("Topic");//"ztesttopic2";
+
+            var config = new ProducerConfig { BootstrapServers = brokerList };
+            int time = 1;
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {                
+                Console.WriteLine("Ctrl-C to quit.\n");
+
+                var cancelled = false;
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true; 
+                    cancelled = true;
+                };
+
+                while (!cancelled)
+                {
+                    Console.Write("> ");
+
+                    string text;
+                    try
+                    {
+                        TimeTable timetable = new TimeTable();
+                        timetable.Populate(time++);
+                        text = timetable.ToString(); 
+                    }
+                    catch (IOException)
+                    {                        
+                        break;
+                    }
+                    if (text == null)
+                    {                     
+                        break;
+                    }
+                    //
+                    string key = "data";
+                    string val = text;                    
+
+                    try
+                    {                      
+                        var deliveryReport = await producer.ProduceAsync(
+                            topicName, new Message<string, string> { Key = key, Value = val });
+
+                        Console.WriteLine($"Delivered to: {deliveryReport.TopicPartitionOffset}");
+                    }
+                    catch (ProduceException<string, string> e)
+                    {
+                        Console.WriteLine($"failed to deliver message: {e.Message} [{e.Error.Code}]");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Failed to deliver message. Error: {ex.ToString()}");
+                    }
+                    Thread.Sleep(500);
+                }
+            }
+        }
+    }
+}
